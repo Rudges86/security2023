0efb114a0da52f34710043bf4732899cc21cfb2f
Rudge Santos <rudges86@gmail.com>
2023-02-02 19:01:08 -0300

    Adicionado o @Configuration, pois ele não vem mais dentro do @EnableWebSecurity

--- src/main/java/com/security/security/config/SecurityConfig.java
+++ src/main/java/com/security/security/config/SecurityConfig.java
@@ -2,11 +2,12 @@ package com.security.security.config;
 
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 //Quando criamos essa config, o spring vai sair da segurança padrão para esta que criamos
